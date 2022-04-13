057955492853d10ae2427c43af720b5f4f826a03
unknown <shravya8132@gmail.com>
2022-04-13 14:52:02 +0530

    marks

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       </header>
     </div>
   );*/
-  const [userInput, setSomething] = useState({
+  /*const [userInput, setSomething] = useState({
     name:"",
     usn:"",
     university:"",
@@ -49,10 +49,87 @@ function App() {
   return(
     <div className="App">
     <input placeholder='Name' onChange={handleNameChange} value={userInput.name}/>
-    <button onClick={handleDataVisibility }></button>
+    <button onClick={handleDataVisibility }> start</button>
     </div>
 
-  );
-}
+  );*/
+  const [userInput1 , setUserInput1]=useState({
+    
+    sst:0,
+    hindi:0
+    
+  })
+  const [userInput2,setUserInput2]=useState({
+    sst:0,
+    hindi:0
+  })
+  const clickme=()=>{
+    if(userInput1.sst>userInput2.sst)
+    {
+      alert("1 wins sst")
+    }
+    else{
+      alert("2 wins sst")
+    }
+  }
 
-export default App;
+  const handleSChange1 = (event) => {   
+    const obj1={
+      sst:event.target.value,
+      hindi:userInput1.hindi
+    }  
+    setUserInput1(obj1) 
+  }
+  
+  const handleSSChange1 = (event) => {   
+    const obj1={
+      
+      sst:userInput1.sst,
+      hindi:event.target.value
+    }  
+    setUserInput1(obj1)   
+  }
+  
+  const handleSSSChange1 = (event) => {   
+    const obj2={
+      sst:event.target.value,
+      hindi:userInput2.hindi
+    }  
+    setUserInput2(obj2) 
+  }
+  
+  const handleSSSSChange1 = (event) => {   
+    const obj2={
+      
+      sst:userInput2.sst,
+      hindi:event.target.value
+    }  
+    setUserInput2(obj2)   
+  }
+  
+  
+    return (
+      <div className="App">
+   <form>
+      
+      <input onChange={handleSChange1} value={userInput1.sst}></input><br></br>
+      <input onChange={handleSSChange1}value={userInput1.hindi}></input><br></br>
+      <input onChange={handleSSSChange1} value={userInput2.sst}></input><br></br>
+      <input onChange={handleSSSSChange1}value={userInput2.hindi}></input><br></br>
+  
+      <button onClick={clickme}>
+      Click me</button>
+     
+    </form> 
+  
+  
+      </div>
+     
+    );
+  }
+  
+  export default App;
+  //camel case
+  //{} instead of ""
+  //5ip-name usn colg age gender--
+
